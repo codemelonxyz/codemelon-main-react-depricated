147364dce6fa8b1a840cd24a1485b21b71075f54
Vaiditya2207 <vaiditya.t23csai@nst.rishihood.edu.in>
2024-11-11 23:10:51 +0530

    Initial Commits

---
+++ README.md
@@ -0,0 +1,7 @@
+# Codemelon
+
+Official Github Repo of @codemelon.xyz
+
+## Updates
+
+Core Work is being done please check again for more updates
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './index.css';
+import { useState } from 'react';
+
+function App() {
+  const [theme, setTheme] = useState('light');
+  return (
+    <div style={{width: "100vw", height: "100vh", fontFamily: "'Sour Gummy', sans-serif"}} className={theme === "light" ? "bg-white flex justify-center items-center text-black flex-col" : "bg-black flex justify-center items-center text-white flex-col"} >
+      <img src={theme === "dark" ? "/assets/logo.png" : "/assets/logowhite.png"} alt="Logo" className="w-96" />
+      <p className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">CodeMelon</p>
+      <p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">We are working the best to make it reality</p>
+      <p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">Check back again for updates</p>
+      <button onClick={() => setTheme(theme === "light" ? "dark" : "light")} className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md">Toggle Theme</button>
+    </div>
+  );
+}
+
+export default App;
